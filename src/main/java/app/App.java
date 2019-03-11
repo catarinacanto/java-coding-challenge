f25dd56e419884bcf6c706c3822bcd0bfb45cc46
Catarina Canto <catarinaccanto@gmail.com>
2019-03-11 01:10:08 +0000

    Added index.html

---
+++ src/main/java/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+import controller.TranslationController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackageClasses = TranslationController.class)
+
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
