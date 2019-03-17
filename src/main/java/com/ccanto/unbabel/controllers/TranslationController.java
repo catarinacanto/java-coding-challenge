d219a066f2f400300ee789cb2e18d79a6f4db5e3
Catarina Canto <catarinaccanto@gmail.com>
2019-03-17 11:42:03 +0000

    sending request

--- src/main/java/com/ccanto/unbabel/controllers/TranslationController.java
+++ src/main/java/com/ccanto/unbabel/controllers/TranslationController.java
@@ -1,17 +1,57 @@
 package com.ccanto.unbabel.controllers;
 
-import com.ccanto.unbabel.services.translationService.TranslationResponse;
 import com.ccanto.unbabel.services.translationService.TranslationService;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import javax.net.ssl.HttpsURLConnection;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class TranslationController {
 
-	@Autowired TranslationService translationService;
+	@Autowired
+	TranslationService translationService;
 
-	@RequestMapping(value = "/translate", method = RequestMethod.POST, produces = {"application/json"})
-	public TranslationResponse getTextToTranslate(@RequestParam(value = "text") String text, @RequestParam(value = "language") String language){
-		return translationService.requestTranslation(text, language);
+
+
+	@RequestMapping(value = "/translate", produces = MediaType.APPLICATION_JSON_VALUE  + ";charset=UTF-8")
+	public void getTextToTranslate(@RequestParam(value = "text") String text, @RequestParam(value = "language") String language) throws IOException, JSONException {
+		URL url = new URL("https://sandbox.unbabel.com/tapi/v2/translation/");
+		HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+		conn.setReadTimeout(30000);
+		conn.setConnectTimeout(15000);
+		conn.setRequestMethod("POST");
+		conn.setDoInput(true);
+		conn.setDoOutput(true);
+		conn.setRequestProperty("Authorization", "ApiKey fullstack-challenge:9db71b322d43a6ac0f681784ebdcc6409bb83359");
+		conn.setRequestProperty("Content-Type", "application/json");
+
+		JSONObject request = new JSONObject();
+		request.put("text", text);
+		request.put("target_language", language);
+		request.put("source_language", "en");
+
+		OutputStream os = conn.getOutputStream();
+		BufferedWriter writer = new BufferedWriter(
+				new OutputStreamWriter(os, "UTF-8"));
+		writer.write(request.toString());
+		writer.flush();
+		writer.close();
+		os.close();
+
+		System.out.println(conn.getInputStream());
+		conn.connect();
 	}
+
+
 }
--- src/main/java/com/ccanto/unbabel/services/translationService/TranslationService.java
+++ src/main/java/com/ccanto/unbabel/services/translationService/TranslationService.java
@@ -1,16 +1,80 @@
 package com.ccanto.unbabel.services.translationService;
 
-import com.ccanto.unbabel.services.AbstractService;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-@Service
-public class TranslationService extends AbstractService{
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-	public TranslationResponse requestTranslation(String text, String targetLanguage){
-		TranslationRequest request = new TranslationRequest();
-		System.out.println(request);
-		initialize();
-		request.setTargetLanguage(targetLanguage).setText(text);
-		return new TranslationResponse();
+@Service
+public class TranslationService {
+
+
+	public void execute(String text, String targetLanguage) {
+		HttpsURLConnection conn;
+		OutputStream outputStream = null;
+		BufferedWriter writer = null;
+		JSONObject request;
+		try {
+			conn = getSandBoxConnection();
+			outputStream = conn.getOutputStream();
+			writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+			request = readRequest(text, targetLanguage);
+			writer.write(request.toString());
+			System.out.println(conn.getInputStream());
+			conn.connect();
+
+		} catch (IOException e) {
+			e.getMessage();
+		} finally {
+			try {
+				if (writer != null) {
+					writer.flush();
+					writer.close();
+				}
+				if (outputStream != null) {
+					outputStream.close();
+				}
+			} catch (IOException e) {
+				e.getMessage();
+			}
+		}
+
 	}
+
+	private HttpsURLConnection getSandBoxConnection() throws IOException {
+		URL url = new URL("https://sandbox.unbabel.com/tapi/v2/translation/");
+		HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+		conn.setReadTimeout(10000);
+		conn.setConnectTimeout(15000);
+		conn.setRequestMethod("POST");
+		conn.setDoInput(true);
+		conn.setDoOutput(true);
+		conn.setRequestProperty("Authorization", "ApiKey fullstack-challenge:9db71b322d43a6ac0f681784ebdcc6409bb83359");
+		conn.setRequestProperty("Content-Type", "application/json");
+		return conn;
+	}
+
+	private JSONObject readRequest(String text, String targetLanguage) {
+		JSONObject request = new JSONObject();
+
+		try {
+			request.put("text", text);
+			request.put("source_language", "en");
+			request.put("target_language", targetLanguage);
+
+		} catch (JSONException e) {
+			e.getMessage();
+		}
+		return request;
+	}
+
+
 }
