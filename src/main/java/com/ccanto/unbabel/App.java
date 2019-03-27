12ab068f40897868f85b70d9af04c98db22e99a9
Catarina Canto <catarinaccanto@gmail.com>
2019-03-27 13:26:09 +0000

    changed connector

--- src/main/java/com/ccanto/unbabel/App.java
+++ src/main/java/com/ccanto/unbabel/App.java
@@ -2,10 +2,11 @@ package com.ccanto.unbabel;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@EnableAutoConfiguration
 public class App {
 
 	public static void main(String[] args) {
